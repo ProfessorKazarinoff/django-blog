dc66d0df4615ba09e9443d0d00e6ca5c72567028
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2020-05-27 12:26:44 -0700

    post detail view almost complete

--- posts/urls.py
+++ posts/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path
 
-from .views import PostListView
+from .views import (
+    PostListView,
+    PostDetailView,
+)
 
 urlpatterns = [
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('', PostListView.as_view(), name='post_list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,12 @@
 # posts/views.py
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 class PostListView(ListView):
     model = Post
     template_name = 'post_list.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
